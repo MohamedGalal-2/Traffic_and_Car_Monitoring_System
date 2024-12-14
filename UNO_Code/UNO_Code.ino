e00ccf3203a4a3f6bc4dc02de54d4f08a25424b9
Mohamed Galal <MohamedAhmedGalal74_sd@eng.bsu.edu.eg>
2024-12-14 15:32:03 +0200

    Added speed profiles and Slef drive function

--- UNO_Code/UNO_Code.ino
+++ UNO_Code/UNO_Code.ino
@@ -13,6 +13,12 @@
 // Set threshold
 const int STOP_DISTANCE = 7;    // Distance to stop the car in cm
 
+// Set car speed profiles
+int SPEED = 255;
+const int HIGH_SPEED = 255;
+const int MEDIUM_SPEED = 170;
+const int LOW_SPEED = 85;
+
 SoftwareSerial HC05(9, 10); // RX, TX
 
 //initial motors pin
@@ -40,10 +46,6 @@ void setup()
 
 void loop() 
 {
-  // Search for black line on the road
-  int rightIRSensorValue = digitalRead(IR_SENSOR_RIGHT);
-  int leftIRSensorValue = digitalRead(IR_SENSOR_LEFT);
-
   // Measure distance of the obstacle
   int obstacleDistance = getDistance();
 
@@ -59,49 +61,39 @@ void loop()
     Serial.println(obstacleDistance); 
   }
 
-   //If none of the sensors detects black line, then go straight
-  if (rightIRSensorValue == LOW && leftIRSensorValue == LOW)
-  {
-    Serial.println("Forward");
-    forward();
-  }
-  //If right sensor detects black line, then turn right
-  else if (rightIRSensorValue == HIGH && leftIRSensorValue == LOW )
-  {
-    right();
-  }
-  //If left sensor detects black line, then turn left  
-  else if (rightIRSensorValue == LOW && leftIRSensorValue == HIGH )
-  {
-    left();
-  } 
-  //If both the sensors detect black line, then stop 
-  else 
-  {
-    stop();
-  }
-
   // Check if there is a Bluetooth connection established
   if (HC05.available() > 0) 
   {
     command = HC05.read();
 
     stop(); //initialize with motors stoped
-    Serial.println(command);
+    Serial.println(command); // Debugging
 
     switch (command) 
     {
+      case 'H'
+        SPEED = HIGH_SPEED;
+        break;
+      case 'M'
+        SPEED = MEDIUM_SPEED;
+        break;
+      case 'N'
+        SPEED = LOW_SPEED;
+        break;
       case 'F':
-        forward();
+        forward(SPEED);
         break;
       case 'B':
-        back();
+        back(SPEED);
         break;
       case 'L':
-        left();
+        left(SPEED);
         break;
       case 'R':
-        right();
+        right(SPEED);
+        break;
+      case 'S':
+        self_Drive(SPEED);
         break;
     }
   }
@@ -119,39 +111,39 @@ void forward()
   motor4.run(FORWARD);  //rotate the motor clockwise
 }
 
-void back()
+void back(int CAR_SPEED)
 {
-  motor1.setSpeed(255); //Define maximum velocity
+  motor1.setSpeed(CAR_SPEED); //Define maximum velocity
   motor1.run(BACKWARD); //rotate the motor anti-clockwise
-  motor2.setSpeed(255); //Define maximum velocity
+  motor2.setSpeed(CAR_SPEED); //Define maximum velocity
   motor2.run(BACKWARD); //rotate the motor anti-clockwise
-  motor3.setSpeed(255); //Define maximum velocity
+  motor3.setSpeed(CAR_SPEED); //Define maximum velocity
   motor3.run(BACKWARD); //rotate the motor anti-clockwise
-  motor4.setSpeed(255); //Define maximum velocity
+  motor4.setSpeed(CAR_SPEED); //Define maximum velocity
   motor4.run(BACKWARD); //rotate the motor anti-clockwise
 }
 
-void left()
+void left(int CAR_SPEED)
 {
-  motor1.setSpeed(255); //Define maximum velocity
+  motor1.setSpeed(CAR_SPEED); //Define maximum velocity
   motor1.run(BACKWARD); //rotate the motor clockwise
-  motor2.setSpeed(255); //Define lower velocity
+  motor2.setSpeed(CAR_SPEED); //Define lower velocity
   motor2.run(FORWARD); //rotate the motor clockwise
-  motor3.setSpeed(255); //Define maximum velocity
+  motor3.setSpeed(CAR_SPEED); //Define maximum velocity
   motor3.run(FORWARD);  //rotate the motor clockwise
-  motor4.setSpeed(255); // Define lower velocity
+  motor4.setSpeed(CAR_SPEED); // Define lower velocity
   motor4.run(FORWARD);  //rotate the motor clockwise
 }
 
 void right()
 {
-  motor1.setSpeed(255); //Define lower velocity
+  motor1.setSpeed(CAR_SPEED); //Define lower velocity
   motor1.run(FORWARD);  //rotate the motor clockwise
-  motor2.setSpeed(255); //Define maximum velocity
+  motor2.setSpeed(CAR_SPEED); //Define maximum velocity
   motor2.run(BACKWARD);  //rotate the motor clockwise
-  motor3.setSpeed(255); //Define lower velocity
+  motor3.setSpeed(CAR_SPEED); //Define lower velocity
   motor3.run(FORWARD); //rotate the motor clockwise
-  motor4.setSpeed(255); //Define maximum velocity
+  motor4.setSpeed(CAR_SPEED); //Define maximum velocity
   motor4.run(FORWARD); //rotate the motor clockwise
 }
 
@@ -178,4 +170,34 @@ int getDistance()
 
   long duration = pulseIn(ECHO_PIN, HIGH);
   return duration * 0.034 / 2; // Convert to cm
+}
+
+void self_Drive(int CAR_SPEED);
+{
+  // Search for black line on the road
+  int rightIRSensorValue = digitalRead(IR_SENSOR_RIGHT);
+  int leftIRSensorValue = digitalRead(IR_SENSOR_LEFT);
+
+   //If none of the sensors detects black line, then go straight
+  if (rightIRSensorValue == LOW && leftIRSensorValue == LOW)
+  {
+    Serial.println("Forward");
+    forward(CAR_SPEED);
+  }
+  //If right sensor detects black line, then turn right
+  else if (rightIRSensorValue == HIGH && leftIRSensorValue == LOW )
+  {
+    right(CAR_SPEED);
+  }
+  //If left sensor detects black line, then turn left  
+  else if (rightIRSensorValue == LOW && leftIRSensorValue == HIGH )
+  {
+    left(CAR_SPEED);
+  } 
+  //If both the sensors detect black line, then stop 
+  else 
+  {
+    stop();
+  }
+
 }
